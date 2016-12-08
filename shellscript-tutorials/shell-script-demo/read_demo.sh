036416e6d54e78b95f024d82ce6c4b6294d25002
h3int2um <h3int2um@gmail.com>
2016-12-08 23:58:55 +0700

    Shell script demo command read

---
+++ shellscript-tutorials/shell-script-demo/read_demo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo -n "Enter some text > "
+read text
+
+echo "You entered: $text"
---
+++ shellscript-tutorials/shell-script-demo/read_demo_option_s.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+echo -n "Enter username: "
+read username
+
+echo -n "Enter password: "
+read -s password
+
+echo -e "\nUsername: $username. Password: $password"
